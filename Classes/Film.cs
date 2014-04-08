dd4a6d37afa945ca903869a3b34ef6a5f15684a3
teddylequette <a1119164@drdrb.net>
2014-04-08 11:01:39 +0200

    Nouveau projet

---
+++ Classes/Film.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Classes
+{
+    public class Film
+    {
+        private String affiche;
+        private String nom;
+
+
+        public Film(String affiche, String nom) {
+            setAffiche(affiche);
+            setNom(nom);
+        
+        }
+
+        public void setAffiche(String affiche) {
+
+            this.affiche = affiche;
+        }
+
+        public void setNom(String nom)
+        {
+
+            this.nom = nom;
+        }
+
+
+        public String getAffiche()
+        {
+
+            return this.affiche ;
+        }
+
+        public String getNom()
+        {
+
+            return this.nom;
+        }
+
+
+
+    }
+}
---
+++ Classes/Salle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Classes
+{
+    public partial class Salle:Film
+    {
+        private int nbPlace;
+        private int nbPlaceTarifNormal;
+        private int nbPlaceTarifReduit;
+        private static int pourcentageReduction=80;
+        private Double prix;
+
+
+        public Double chiffreAffaires() {
+
+            return this.prix * (this.nbPlace + this.nbPlaceTarifNormal + this.nbPlaceTarifReduit);
+        }
+
+        public void remiseAZero() {
+
+            this.nbPlace = 0;
+            this.nbPlaceTarifNormal = 0;
+            this.nbPlaceTarifReduit = 0;
+        
+        }
+    }
+}
